939076a48ac883cf7f62c86d8b976e972a204130
david.daso <david.daso@sterling.ng>
2022-03-24 15:26:01 -0500

    mmm

--- upload.js
+++ upload.js
@@ -1,2 +1,30 @@
 const fs = require('fs');
 const AWS = require('aws-sdk');
+require('dotenv').config();
+const {ID, SECRET } = process.env;
+const BUCKET_NAME = 'test-bucket';
+
+const s3 = new AWS.S3({
+    accessKeyId: ID,
+    secretAccessKey: SECRET
+});
+
+const uploadFile = (fileName) => {
+    // Read content from the file
+    const fileContent = fs.readFileSync(fileName);
+
+    // Setting up S3 upload parameters
+    const params = {
+        Bucket: BUCKET_NAME,
+        Key: 'cat.jpg', // File name you want to save as in S3
+        Body: fileContent
+    };
+
+    // Uploading files to the bucket
+    s3.upload(params, function(err, data) {
+        if (err) {
+            throw err;
+        }
+        console.log(`File uploaded successfully. ${data.Location}`);
+    });
+};
